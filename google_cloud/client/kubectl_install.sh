4ebdd5f10cb56e2509c34492a08158ab7d206b92
nithyanantha <info@nithya.org>
2018-01-16 15:19:44 +0530

    Update kubectl_install.sh

--- google_cloud/client/kubectl_install.sh
+++ google_cloud/client/kubectl_install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-wget https://storage.googleapis.com/kubernetes-release/release/v1.4.4/bin/linux/amd64/kubectl
+wget https://storage.googleapis.com/kubernetes-release/release/v1.8.5/bin/linux/amd64/kubectl
 
 chmod +x kubectl
 
